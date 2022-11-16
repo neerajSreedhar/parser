9ed6d8e5b69e5cec8d24b890c19617f7fec7ba18
neerajSreedhar <115201376+neerajSreedhar@users.noreply.github.com>
2022-11-16 15:22:44 -0700

    Update model.py
    Concatenated the tensors

--- model.py
+++ model.py
@@ -2,11 +2,27 @@ import clip
 import torch
 from PIL import Image
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+class Network(torch.nn.Module):
+    def __init__(self, clip_model='ViT-B/32', device='cpu'):
+        super().__init__()
+        self.device = device
+        model, preprocess = clip.load(clip_model, device=self.device)
+        self.clipModel = model
+        self.preprocess = preprocess
 
-if __name__ == '__main__':
-    #print(device)
+    
+    def forward(self, x):
+        image = self.preprocess(x['img']).unsqueeze(0).to(self.device)
+        text = clip.tokenize([x['instruction']]).to(self.device)
 
+        with torch.no_grad():
+            image_features = self.clipModel.encode_image(image)
+            text_features = self.clipModel.encode_text(text)
+
+        concat = torch.cat((image_features, text_features), dim=1)
+        return concat 
+
+def test_clip():
     model, preprocess = clip.load('ViT-B/32', device=device)
     image = preprocess(Image.open("dog.jpeg")).unsqueeze(0).to(device)
     text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
@@ -20,3 +36,13 @@ if __name__ == '__main__':
     print(f'image features shape: {image_features.shape}')
     print(f'text features shape: {text_features.shape}')
     print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
+
+if __name__ == '__main__':
+    #print(device)
+    #test_clip()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    img = Image.open('dog.jpeg')
+    instruction = 'it is a dog'
+    model = Network(device=device)
+    
+    print(model({'img':img, 'instruction':instruction}).shape)
